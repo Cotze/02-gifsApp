07b399aa138371ea5fc27f59439fecf4f08050d5
Cotze <carlosrodigoatl@gmail.com>
2022-01-03 07:01:12 -0600

    Control del historial de busqueda

---
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { GifsService } from '../../gifs/services/gifs.service';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styles: [
+  ]
+})
+export class SidebarComponent {
+
+  constructor( 
+    private gifsServices: GifsService ) { }
+
+  get historial(){
+    return this.gifsServices.historial;
+  }
+
+}
